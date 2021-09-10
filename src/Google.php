e277739803ee5c685dbc9eed5c0ff319912b4ca9
pangxianfei <421339244@qq.com>
2021-09-10 11:32:53 +0800

    v1.0

---
+++ src/Google.php
@@ -0,0 +1,120 @@
+<?php
+namespace Tmaic\Translate;
+
+use GuzzleHttp\Client;
+use Tmaic\Translate\Exceptions\TranslateException;
+use Stichoza\GoogleTranslate\TranslateClient;
+
+class Google implements TranslateInterface
+{
+
+    protected static $language = [
+        '' =>'auto',   //中文
+        'auto' =>'auto',   //中文
+        'zh' =>'zh-CN',   //中文
+        'hk' =>'zh-TW',   //繁体
+        'en' =>'EN',   //英文
+        'jp' =>'jp',   //日文
+        'ko' =>'kor',  //韩文
+        'fr' =>'fra',  //法语
+        'ru' =>'ru',   //俄语
+        'es' =>'spa',  //西班牙语
+        'pt' =>'pt',  //葡萄牙语
+    ];
+
+
+
+    private $app_id;
+    private $app_key;
+    private $base_url;
+    private $options;
+    private $httpClient;
+    private $from;
+    private $to;
+    public $source = false;
+
+
+    public function __construct($app_id,$app_key,$from,$to,$base_url,$options=[])
+    {
+        $this->app_id = $app_id;
+        $this->app_key = $app_key;
+        $this->from = $this->checkLanguage($from);
+        $this->to = $this->checkLanguage($to);
+        $this->base_url = $base_url;
+        $this->options = $options;
+    }
+
+
+    /**
+     * @author Tmaic
+     * @email szm19920426@gmail.com
+     * @param $string
+     * @param bool $source  返回原数据结构
+     * @return mixed
+     */
+    public function translate($string,$source=false)
+    {
+        $driver = new TranslateClient( $this->from,  $this->to,$this->options);
+        $driver->setUrlBase($this->base_url);
+        if($source){
+           $result= $driver->getResponse($string);
+        }else{
+            $result=  $driver->translate($string);
+        }
+        return $result;
+
+    }
+
+
+
+
+    /**
+     * @author Tmaic
+     * @email szm19920426@gmail.com
+     * @param $language
+     * @return mixed
+     * @throws TranslateException
+     */
+    private static function checkLanguage($language)
+    {
+
+        if (!isset(self::$language[$language])) {
+            throw new TranslateException('10000');
+        }
+
+        return self::$language[$language];
+    }
+
+
+
+
+
+
+
+    /**
+     * @author Tmaic
+     * @email szm19920426@gmail.com
+     * @param $attr
+     * @param $value
+     * @return $this
+     */
+    public function __set($attr,$value)
+    {
+        $this->$attr = $value;
+        return $this;
+    }
+
+
+    /**
+     * @author Tmaic
+     * @email szm19920426@gmail.com
+     * @param $attr
+     * @return mixed
+     */
+    public function __get($attr)
+    {
+        return $this->$attr;
+    }
+
+
+}
---
+++ src/TranslateService.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace Tmaic\Translate;
+
+
+use Tmaic\Translate\Exceptions\TranslateException;
+
+class TranslateService implements TranslateInterface
+{
+
+
+
+    private $driver;
+    private $spare_driver;
+    private $config =[];
+    private $options=[];
+    private $from;
+    private $to;
+    public $source = false;
+
+
+
+
+    public function __construct($config=[])
+    {
+        $this->config=$config;
+        if(!count($config)){
+            $this->config = include(__DIR__.'/../config/translate.php');
+        }
+
+        $this->driver = $this->config['defaults']['driver'];
+        $this->spare_driver = $this->config['defaults']['spare_driver'];
+        $this->from = $this->config['defaults']['from'];
+        $this->to = $this->config['defaults']['to'];
+    }
+
+
+
+    public function setDriver($driver){
+
+        $this->driver = $driver;
+        return $this;
+    }
+
+
+
+    public function setFromAndTo($from,$to){
+
+        $this->from = $from;
+        $this->to = $to;
+        return $this;
+    }
+
+
+    public function setHttpOption($options = []){
+        $this->options = $options;
+        return $this;
+    }
+
+
+    /**
+     * 执行翻译
+     * @author Tmaic
+     * @email szm19920426@gmail.com
+     * @param $string
+     * @param bool $source 原数据，针对google
+     * @return mixed
+     * @throws TranslateException
+     */
+    public function translate($string,$source=false)
+    {
+        $this->source=$source;
+        try {
+            return $this->sendTranslate($string, $this->driver);
+        } catch (\Exception $e) {
+            //自动切换为备用渠道
+            return $this->sendTranslate($string, $this->spare_driver);
+        }
+    }
+
+
+    /**
+     * 执行请求
+     * @author Tmaic
+     * @email szm19920426@gmail.com
+     * @param $string
+     * @param $driver
+     * @return mixed
+     * @throws TranslateException
+     */
+    protected function sendTranslate($string,$driver){
+
+        $appKey = $this->config['drivers'][$driver]['app_key'];
+        $appId = $this->config['drivers'][$driver]['app_id'];
+        $baseUrl = $this->config['drivers'][$driver]['base_url'];
+        switch ($driver){
+            case 'baidu':
+                $obj = new Baidu($appId,$appKey,$this->from,$this->to,$baseUrl,$this->options);
+                break;
+            case 'youdao':
+                $obj = new YouDao($appId,$appKey,$this->from,$this->to,$baseUrl,$this->options);
+                break;
+            case 'google':
+                $obj = new Google($appId,$appKey,$this->from,$this->to,$baseUrl,$this->options);
+                break;
+            default:
+                throw new TranslateException(10003);
+        }
+
+        return $obj->translate($string, $this->source);
+    }
+
+
+
+    /**
+     * @author Tmaic
+     * @email szm19920426@gmail.com
+     * @param $attr
+     * @param $value
+     * @return $this
+     */
+    public function __set($attr,$value)
+    {
+        $this->$attr = $value;
+        return $this;
+    }
+
+
+    /**
+     * @author Tmaic
+     * @email szm19920426@gmail.com
+     * @param $attr
+     * @return mixed
+     */
+    public function __get($attr)
+    {
+        return $this->$attr;
+    }
+
+
+
+}
